f0c0c2245d81e019bc0487814651b277b9599572
karthikdurai <iamkarthiksiddarth@gmail.com>
2024-07-04 16:38:55 +0530

    move to env

--- src/utils/getNonceForArcanaSponsorship.ts
+++ src/utils/getNonceForArcanaSponsorship.ts
@@ -1,11 +1,14 @@
 import { ethers, WebSocketProvider } from "ethers";
 
+const VITE_APP_CONTRACT_ADDRESS_FOR_NONCE = import.meta.env
+  .VITE_APP_CONTRACT_ADDRESS_FOR_NONCE;
+
 async function getNonceForArcanaSponsorship(
   address: string,
   rpcUrl: string
 ): Promise<ethers.BigNumberish> {
   const c = new ethers.Contract(
-    "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789",
+    VITE_APP_CONTRACT_ADDRESS_FOR_NONCE,
     [
       {
         inputs: [
